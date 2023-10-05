d4747964940af7617f22a5e1b43ca664f8e2b3df
Samuel Sukovský <145551742+SamuelSukovsky@users.noreply.github.com>
2023-10-05 11:03:04 +0100

    Walk (almost)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D body;
+    private Animator anim;
+    private float dir;
+    public float speed = 2f;
+
+    void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        PlayerInput input = GetComponent<PlayerInput>();
+        input.actions["AD"].performed += Run;
+    }
+
+    private void Run(InputAction.CallbackContext context)
+    {
+        dir = context.ReadValue<float>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ()
+        body.velocityX = dir * speed;
     }
 }
